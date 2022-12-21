4b75350e8ec8c772324e75579c185e13e1679cb6
AKOWSPC <akows141@gmail.com>
2022-12-21 14:49:14 +0900

    [로그인 페이지] - 로그인 폼 UI 구현. - 로그인 폼 반응형 대응.

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -2,9 +2,34 @@ import './Login.css';
 import '../ResetStyle.css';
 
 const Login = () => {
+
+    const submitEvent = () => {
+        return;
+    };
+
     return (
         <div className='login'>
-            Login
+
+            <form>
+                <div className='loginform'>
+
+                    <p>Login</p>
+
+                    <div className='input'>
+                        <input/>
+                    </div>
+
+                    <div className='input'>
+                        <input/>
+                    </div>
+
+                    <div className='submitbutton' onClick={submitEvent}>
+                        로그인
+                    </div>
+
+                </div>
+            </form>
+
         </div>
     );
 };
