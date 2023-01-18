3a83173669c2dc98fa1b7697d0723eab503cd8a3
AKOWSPC <akows141@gmail.com>
2023-01-18 18:24:39 +0900

    [공부기록 페이지] [기능 개선] - 개선된 페이지네이션 기능 구현을 완료함. - 화면에 출력할 부분만 가져오는 기능을 구현했으며 페이지 당 보여질 아이템 갯수를 제어할 로직도 구현하였음. - 실제 기능 구현에 맞춰 리액트 Hook을 활용하여 각 변수들을 능등적으로 제어하도록 전체 코드를 수정함.

--- frontend/src/Pages/Test.js
+++ frontend/src/Pages/Test.js
@@ -4,7 +4,7 @@ import { useContext, useEffect, useState } from 'react';
 
 import { FirebaseContext } from '../App';
 
-import { collection, doc, getCountFromServer, getDoc, getDocs, limit, orderBy, query, startAfter } from 'firebase/firestore';
+import { collection, doc, getCountFromServer, getDoc, getDocs, limit, orderBy, query, startAfter, startAt } from 'firebase/firestore';
 
 const Test = () => {
 
@@ -15,27 +15,18 @@ const Test = () => {
 
 
 
-    // 출력용 data를 제어할 useState.
+    // 출력용 data를 제어하는 useState. 초기값은 빈 배열.
     const [data, setData] = useState([]);
 
+    // 페이지 당 출력할 데이터의 갯수를 제어할 useState. 초기값은 12.
+    const [itemsPerPage, setItemsPerPage] = useState(12);
 
+    // 데이터의 전체 Langth를 제어하는 useState. 초기값은 0.
+    const [dataLangth, setDataLangth] = useState(0);
 
-
-
-    // collection의 전체 길이를 구하는 메소드.
-    const checkRecordLangth = async () => {
-        console.log('checkRecordLangth active.');
-
-        // collection 특정, query문 날리기.
-        const collectionRef = collection(firebaseContext.fireStoreDB, 'MyStudyRecord');
-        const querys = query(collectionRef, orderBy('Create_date', 'desc')); 
-
-        // 해당하는 data의 전체 Count를 구하기.
-        const getCounts = await getCountFromServer(querys);
-
-        // 반환받은 data의 실제 data 부분의 Count를 계산.
-        console.log('recordLangth is : ' + getCounts.data().count);
-    };
+    // 데이터의 첫 위치와 마지막 위치를 제어하는 useState. 초기값은 빈 배열.
+    const [firstDoc, setFirstDoc] = useState([]);
+    const [lastDoc, setLastDoc] = useState([]);
 
 
 
@@ -88,29 +79,15 @@ const Test = () => {
 
 
 
-    // queryPage.
-    const queryPage = async () => {
-        console.log('queryPage active.');
+    // queryPage 메소드들.
+    const queryPagePrev = async () => {
+        console.log('queryPagePrev active.');
 
         // collection 특정, query문 날리기. (여기서는 12개 데이터만 가져오도록 설정.)
         const collectionRef = collection(firebaseContext.fireStoreDB, 'MyStudyRecord');
-        const querys = query(collectionRef, orderBy('Create_date', 'desc'), limit(12)); 
-
-        // 해당하는 Docs 모두 가져오기.
-        const documentSnapshots = await getDocs(querys);
-
-        // 처음과 마지막에 해당하는 Docs의 값을 제어하도록.
-        // const firstVisible = documentSnapshots.docs[0];
-        const lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
-
-        // 정상적으로 데이터를 불러오는지 확인하는 구문. (정상 작동 확인함.)
-        // console.log(firstVisible.data());
-        // console.log(lastVisible.data());
-
-
 
         // 마지막에 해당하는 Docs의 값을 기준으로 그 이후 Docs를 가져오는 쿼리문.
-        const nextDocsQuery = query(collectionRef, orderBy('Create_date', 'desc'), startAfter(lastVisible), limit(12));
+        const nextDocsQuery = query(collectionRef, orderBy('Create_date', 'asc'), startAt(lastDoc), limit(itemsPerPage));
 
         // 해당하는 Docs 모두 가져오기.
         const nextDocumentSnapshots = await getDocs(nextDocsQuery);
@@ -121,20 +98,104 @@ const Test = () => {
             ...doc.data()
         }));
 
-        // 풀려진 Docs를 변수에 대입.
-        setData(mappingData);
+        // 풀려진 Docs를 출력.
+        console.log(mappingData);
+    };
 
-        console.log(data);
+    const queryPageNext = async () => {
+        console.log('queryPageNext active.');
+
+        // collection 특정, query문 날리기. (여기서는 12개 데이터만 가져오도록 설정.)
+        const collectionRef = collection(firebaseContext.fireStoreDB, 'MyStudyRecord');
+
+        // 마지막에 해당하는 Docs의 값을 기준으로 그 이후 Docs를 가져오는 쿼리문.
+        const nextDocsQuery = query(collectionRef, orderBy('Create_date', 'desc'), startAfter(lastDoc), limit(itemsPerPage));
+
+        // 해당하는 Docs 모두 가져오기.
+        const nextDocumentSnapshots = await getDocs(nextDocsQuery);
+
+        // 받아온 Docs를 map 함수를 이용하여 배열에 풀어놓기.
+        const mappingData = nextDocumentSnapshots.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+
+        // 풀려진 Docs를 출력.
+        console.log(mappingData);
     };
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+    // collection의 전체 길이를 구하는 메소드. (useEffect로 자동 실행.)
+    const setRecordLangth = async () => {
+        // collection 특정, query문 날리기.
+        const collectionRef = collection(firebaseContext.fireStoreDB, 'MyStudyRecord');
+        const querys = query(collectionRef, orderBy('Create_date', 'desc')); 
+
+        // 해당하는 data의 전체 Count를 구하기.
+        const getCounts = await getCountFromServer(querys);
+
+        // 반환받은 data의 실제 data 부분의 Count를 계산하여 변수에 대입하고 콘솔에 출력.
+        setDataLangth(getCounts.data().count);
+    };
+
+    // 페이지네이션을 위한 변수값 선언 메소드. (itemsPerPage는 12.)
+    const setPagenationValue = async () => {
+        // collection 특정, query문 날리기. 
+        const collectionRef = collection(firebaseContext.fireStoreDB, 'MyStudyRecord');
+        const querys = query(collectionRef, orderBy('Create_date', 'desc'), limit(itemsPerPage)); 
+
+        // 해당하는 Docs 모두 가져오기.
+        const documentSnapshots = await getDocs(querys);
+
+        // 처음과 마지막에 해당하는 Docs의 값을 계산하여 set.
+        setFirstDoc(documentSnapshots.docs[0]);
+        setLastDoc(documentSnapshots.docs[documentSnapshots.docs.length - 1]);
+    };
+
     useEffect(() => {
         const titleElement = document.getElementsByTagName("title")[0];
         titleElement.innerHTML = '테스트 페이지';
+
+        // 컴포넌트가 출력되면 모든 데이터의 길이를 받아와서 변수에 대입.
+        setRecordLangth();
+
+        // 화면에 출력할 특정 갯수의 데이터의 처음과 끝 위치를 변수에 대입.
+        setPagenationValue();
+
+        // eslint-disable-next-line
     }, []);
+
+
+
+    const getFunctionValue = () => {
+        console.log('itemsPerPage : ' + itemsPerPage);
+        console.log('dataLangth : ' + dataLangth);
+        console.log('firstDocTitle : ' + firstDoc.data().Title);
+        console.log('lastDocTitle : ' + lastDoc.data().Title);
+    };
+
+    const setItemsPerPageNumber6 = () => {
+        setItemsPerPage(6);
+    };
+    const setItemsPerPageNumber12 = () => {
+        setItemsPerPage(12);
+    };
+
+
 
     return (
         <div className='home'>
@@ -145,8 +206,8 @@ const Test = () => {
                 <br/>
                 <br/>
 
-                <button onClick={checkRecordLangth}>
-                    checkRecordLangth
+                <button onClick={getFunctionValue}>
+                    getFunctionValue
                 </button>
 
                 <br/>
@@ -166,8 +227,21 @@ const Test = () => {
                 <br/>
                 <br/>
 
-                <button onClick={queryPage}>
-                    queryPage
+                <button onClick={queryPagePrev}>
+                    queryPagePrev
+                </button>
+                <button onClick={queryPageNext}>
+                    queryPageNext
+                </button>
+
+                <br/>
+                <br/>
+
+                <button onClick={setItemsPerPageNumber6}>
+                    get6
+                </button>
+                <button onClick={setItemsPerPageNumber12}>
+                    get12
                 </button>
 
             </div>
